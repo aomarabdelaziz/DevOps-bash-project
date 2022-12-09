4f07d4dc6b9f6edc44b48fe9a850dc32cf384669
mahmouddahaby <mahmoud.dahaby7@gmail.com>
2022-12-09 23:19:41 +0200

    Adding listing feature

--- main_menu/list_db.sh
+++ main_menu/list_db.sh
@@ -2,3 +2,28 @@
 
 # using sourcing
 . ./helpers/functions.sh
+
+# to list the data base and make the customer select
+
+select choice in `cat notes`
+do
+    case $REPLY in
+    1)
+        ls Database
+        break
+        ;;
+    2)
+        ls Database
+        cd Database
+        read -p "What is table do you want?" table 
+        if [[ -e $table ]];
+        then
+            ls $table 
+            break
+        else
+            echo "Table doesn't exist"
+            break
+        fi
+        ;;
+    esac
+done
